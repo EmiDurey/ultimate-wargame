d0d018565601c8fe38ca1b4419918e38c8b1f2c8
tdalesme <t.dalesme@laposte.net>
2019-04-25 20:29:05 +0200

    Correction normes SUN

--- src/Model/Hex.java
+++ src/Model/Hex.java
@@ -1,133 +1,131 @@
+package model;
+
 import java.lang.Math;
 
-
+/**
+ * Représente un hexagone sur la map.
+ * On utilise un système de coordonnées cubiques pour le stockage.
+ * On stocke donc notamment les coordonnées de l'hexagone sur la map.
+ */
 public class Hex {
+
 	/**
+	 * Coordonnée x.
+	 */
+	private int x;
 
-         * ReprÃ©sente un hexagone sur la map.
-         * On utilise un systÃ¨me de coordonnÃ©es cubiques pour le stockage.
-		 * On stocke donc notamment les coordonnÃ©es de l'hexagone sur la map.
-         */
-	
-
-	public int x;
-	public int y;
-	public int z;
-
-
-	
 	/**
-         * Constructeur utilisant les coordonnÃ©es cubiques.
-		 * @param newX newY newZ
-		 * @return Hex
-		 * @throws IllegalArgumentException Si la somme des coordonnÃ©es est diffÃ©rente de 0.
-         */
+	 * Coordonnée y.
+	 */
+	private int y;
+
+	/**
+	 * Coordonnée z.
+	 */
+	private int z;
+
+	/**
+     * Constructeur utilisant les coordonnées cubiques.
+	 * @param newX coordonnée x
+	 * @param newY coordonnée y
+	 * @param newZ coordonnée z
+	 * @throws IllegalArgumentException Si la somme des coordonnées
+	 * est différente de 0.
+     */
 	Hex(int newX, int newY, int newZ) {
 
-		if(newX + newY + newZ != 0) {
+		if (newX + newY + newZ != 0) {
 			throw new IllegalArgumentException("x+y+z is not null");
 		}
-		
+
 		x = newX;
 		y = newY;
 		z = newZ;
 	}
 
-
 	/**
-		* Constructeur utilisant les coordonnÃ©es axiales.
-		* Les coordonnÃ©es sont converties en cubique pour le stockage.
-		* @param newX newY
-		* @return Hex
-		*/
+	* Constructeur utilisant les coordonnées axiales.
+	* Les coordonnées sont converties en cubique pour le stockage.
+	* @param newX coordonnée x
+	* @param newY coordonnée y
+	*/
 	Hex(int newX, int newY) {
 		x = newX;
 		y = newY;
-		z = - newX - newY;
+		z = -newX - newY;
 	}
 
-
-	
 	/**
-		* Calcule la somme de 2 Hex
-		* @param b
-		* @return Hex
-		*/
+	* Calcule la somme de 2 hexagones.
+	* @param b hexagone
+	* @return Hex
+	*/
 	public Hex add(Hex b) {
-		 return new Hex(x+b.x, y+b.y, z+b.z);
+		 return new Hex(x + b.x, y + b.y, z + b.z);
 	}
 
-	
 	/**
-		* Calcule la diffÃ©rence de 2 Hex.
-		* @param b
-		* @return Hex
-		*/
+	* Calcule la différence de 2 hexagones.
+	* @param b hexagone
+	* @return Hex
+	*/
 	public Hex substract(Hex b) {
-		 return new Hex(x-b.x, y-b.y, z-b.z);
+		 return new Hex(x - b.x, y - b.y, z - b.z);
 	}
 
-	
 	/**
-		* Calcule le produit d'un Hex par un entier.
-		* @param k
-		* @return Hex
-		*/
+	* Calcule le produit d'un hexagone par un entier.
+	* @param k entier
+	* @return Hex
+	*/
 	public Hex multiply(int k) {
-		return new Hex(k*x, k*y, k*z);
+		return new Hex(k * x, k * y, k * z);
 	}
 
-	
 	/**
-		 * Teste l'Ã©galitÃ© entre 2 Hex.
-		 * @param b
-		 * @return Boolean
-		 */
+	 * Teste l'égalité entre 2 Hex.
+	 * @param b hexagone
+	 * @return Boolean
+	 */
 	public Boolean equals(Hex b) {
-		if (x == b.x && y ==b.y && z == b.z) {
+		if (x == b.x && y == b.y && z == b.z) {
 			return true;
-		}
-		
-		else {
+		} else {
 			return false;
 		}
 	}
 
-
 	/**
-		 * Retourne la distance Ã  0 (l'origine) de l'hexagone.
-		 * @return int
-		 */
+	 * Retourne la distance à 0 (l'origine) de l'hexagone.
+	 * @return int
+	 */
 	public int zeroDistance() {
-		return Math.abs( (x + y + z) / 2 );
+		return Math.abs((x + y + z) / 2);
 	}
 
-
 	/**
-		 * Calcule la distance entre deux hexagones.
-		 * @param b
-		 * @return int
-		 */
+	 * Calcule la distance entre deux hexagones.
+	 * @param b hexagone
+	 * @return int
+	 */
 	public int distance(Hex b) {
 		return substract(b).zeroDistance();
 	}
 
-
 	/**
-		 * Indique si les deux hexagones sont adjacents (cas particulier de "distance").
-		 * @param b
-		 * @return Boolean
-		 */
+	 * Indique si les deux hexagones sont adjacents
+	 * (cas particulier de "distance").
+	 * @param b hexagone
+	 * @return Boolean
+	 */
 	public Boolean isNeighbour(Hex b) {
-		if(distance(b) == 1) {
+		if (distance(b) == 1) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
 
 	//TODO getNeighbour() en fonction de la direction
 	//		Stockage de map, ...
-	
 };
