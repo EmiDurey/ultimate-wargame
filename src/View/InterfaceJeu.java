2d2830fbed327f3f7a8a14f69d8930931099702e
EmiDurey <emi.durey@gmail.com>
2019-05-19 12:22:07 +0200

    vue : ajout menu principal + délimitation des espaces du PanelInformations

--- src/View/InterfaceJeu.java
+++ src/View/InterfaceJeu.java
@@ -1,20 +1,16 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 
 /**
  *  Class InterfaceJeu.
  */
-public class InterfaceJeu extends JFrame implements ActionListener {
+public class InterfaceJeu extends JFrame {
 
 	/**
 	 * Longueur de la fenêtre.
@@ -25,123 +21,98 @@ public class InterfaceJeu extends JFrame implements ActionListener {
 	 * Largeur de la fenêtre.
 	 */
 	private final int largeur = 1000;
+	
+	/**
+	 * Image de fond.
+	 */
+	private JLabel fond;
+	
+	/**
+	 * Panel d'accueil.
+	 */
+	private PanelAccueil panelAccueil;
+
+	
+	/**
+	 * Panel de la carte.
+	 */
+	private PanelCarte panelCarte;
+	
+	
+	/**
+	 * Panel des informaitons du jeu.
+	 */
+	private PanelInformations panelInformations;
+	
+	
+	/**
+	 * Barre de menu.
+	 */
+	private BarreMenu barreMenu;
 
 	/**
 	 *  Construit un objet de type InterfaceJeu.
 	 *  @param titre titre de l'InterfaceJeu
-	 *  @see Carte
-	 *  @see InformationJeu
 	 */
-	public InterfaceJeu(String titre) {
+	public InterfaceJeu(String titre) {	
 		super(titre);
+		this.panelCarte = new PanelCarte();
+		this.panelAccueil = new PanelAccueil(this);
+		this.panelInformations = new PanelInformations();
+		this.barreMenu = null;
+		
 		this.setSize(longueur, largeur);
-		this.setJMenuBar(this.createMenuBar());
-	    
-		Carte carte = new Carte();
-		this.add(carte, BorderLayout.WEST);
-	    
-	    InformationJeu information = new InformationJeu();
-	    this.add(information, BorderLayout.EAST);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+
+		this.setContentPane(this.creerFond());
+		this.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+	    this.getContentPane().add(this.panelAccueil);
 	    
 	    this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-
+	
 	/**
-	 *  Crée la barre de menu.
-	 *  @return JMenuBar
+	 *  Retourne le panel de la carte.
+	 *  @return PanelCarte
 	 */
-	private JMenuBar createMenuBar() {
-
-		String chemin = "images\\JMenuBar\\";
-
-		JMenuBar menuBar = new JMenuBar();
-
-        JMenu menuFichier = new JMenu("Fichier");
-
-        JMenuItem nouvellePartie = new JMenuItem("Nouvelle partie");
-        nouvellePartie.setIcon(new ImageIcon(chemin + "new.png"));
-		nouvellePartie.addActionListener((ActionListener) this);
-		nouvellePartie.setActionCommand("Nouvelle partie");
-		menuFichier.add(nouvellePartie);
-
-		menuFichier.addSeparator();
-
-        JMenuItem ouvrir = new JMenuItem("Ouvrir sauvegarde");
-        ouvrir.setIcon(new ImageIcon(chemin + "open.png"));
-        ouvrir.addActionListener((ActionListener) this);
-        ouvrir.setActionCommand("Ouvrir sauvegarde");
-        menuFichier.add(ouvrir);
-
-        JMenuItem sauvegarder = new JMenuItem("Sauvegarder");
-        sauvegarder.setIcon(new ImageIcon(chemin + "save.png"));
-        sauvegarder.addActionListener((ActionListener) this);
-        sauvegarder.setActionCommand("Sauvegarde");
-        menuFichier.add(sauvegarder);
-
-        menuFichier.addSeparator();
-
-        JMenuItem quitter = new JMenuItem("Quitter");
-        quitter.setIcon(new ImageIcon(chemin + "exit.png"));
-        quitter.addActionListener((ActionListener) this);
-        quitter.setActionCommand("Quitter");
-        menuFichier.add(quitter);
-
-        menuBar.add(menuFichier);
-
-        JMenu menuAide = new JMenu("Aide");
-
-        JMenuItem regles = new JMenuItem("Règles");
-        regles.setIcon(new ImageIcon(chemin + "rules.png"));
-		regles.addActionListener((ActionListener) this);
-		regles.setActionCommand("Règles");
-		menuAide.add(regles);
-
-		JMenuItem aide = new JMenuItem("Aide");
-		aide.setIcon(new ImageIcon(chemin + "help.png"));
-		aide.addActionListener((ActionListener) this);
-		aide.setActionCommand("Aide");
-		menuAide.add(aide);
-
-        menuBar.add(menuAide);
-
-        return menuBar;
+	public PanelCarte getPanelCarte() {
+		return this.panelCarte;
 	}
-
+	
 	/**
-	 *  Permet le traitement des évènements.
-	 *  @param evt évènement
+	 *  Retourne le panel des informations.
+	 *  @return PanelInformations
 	 */
-	public void actionPerformed(ActionEvent evt) {
-
-		String actionCommand = evt.getActionCommand();
-
-		switch (actionCommand) {
-			case "Nouvelle partie":
-				System.out.println("Nouvelle partie");
-				break;
-			case "Ouvrir sauvegarde":
-				System.out.println("Ouvrir sauvegarde");
-				break;
-			case "Sauvegarder":
-				System.out.println("Sauvegarder");
-				break;
-			case "Règles":
-				System.out.println("Règles");
-				break;
-			case "Aide":
-				System.out.println("Aide");
-				break;
-			case "Quitter":
-				int reponse = JOptionPane.showConfirmDialog(this,  "Etes-vous sûr de vouloir quitter le jeu ?", "Confirmation", JOptionPane.OK_CANCEL_OPTION);
-	        	switch (reponse) {
-	        		case JOptionPane.CLOSED_OPTION: break;
-	        		case JOptionPane.CANCEL_OPTION: break;
-	        		case JOptionPane.OK_OPTION:
-	        			System.exit(0);
-	        			break;
-	        	}
-		}
+	public PanelInformations getPanelInformations() {
+		return this.panelInformations;
+	}
+	
+	/**
+	 *  Modifie la barre de menu.
+	 *  @param barreMenu BarreMenu
+	 */
+	public void setBarreMenu(BarreMenu barreMenu) {
+		this.barreMenu = barreMenu;
+		this.setJMenuBar(this.barreMenu);
+	}
+	
+	/**
+	 *  Crée l'image de fond.
+	 *  @return JLabel
+	 */
+	public JLabel creerFond() {
+		this.fond = new JLabel(new ImageIcon("images\\carte.png")) {
+			  public void paintComponent(Graphics g) {
+			    super.paintComponent(g);
+			    if(getIcon() != null)
+			      g.drawImage(
+			          ((ImageIcon)getIcon()).getImage(), 0, 0, 
+			          getWidth(), getHeight(), null);
+			  }
+		};
+		return fond;
 	}
 
 	/**
